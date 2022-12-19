34d3b9fa46fe1eda377ba8ee9472d20ccfc1a960
payam rastogi <payamrastogi@outlook.com>
2022-12-18 22:43:26 -0500

    Added move display logic

--- client.py
+++ client.py
@@ -22,7 +22,41 @@ def redraw_window(win, game, player):
         win.blit(text, (80, 200))
 
         text = font.render("Opponent", 1, (0, 255, 255))
-        win.blit(text, (380, 200))
+        win.blit(text, (380 , 200))
+
+        move1 = game.get_player_move(0)
+        move2 = game.get_player_move(1)
+
+        if game.both_players_move_locked():
+            text1 = font.render(move1, 1, (0, 0, 0))
+            text2 = font.render(move2, 1, (0, 0, 0))
+        else:
+            if game.p1_move_locked and player == 0:
+                text1 = font.render(move1, 1, (0, 0, 0))
+            elif game.p1_move_locked:
+                text1 = font.render("Locked in", 1, (0, 0, 0))
+            else:
+                text1 = font.render("Waiting", 1, (0, 0, 0))
+
+            if game.p2_move_locked and player == 1:
+                text2 = font.render(move2, 1, (0, 0, 0))
+            elif game.p2_move_locked:
+                text2 = font.render("Locked in", 1, (0, 0, 0))
+            else:
+                text2 = font.render("Waiting", 1, (0, 0, 0))
+
+            if player == 1:
+                win.blit(text2, (100, 350))
+                win.blit(text1, (400, 350))
+            else:
+                win.blit(text1, (100, 350))
+                win.blit(text2, (400, 350))
+        for button in buttons:
+            button.draw(win)
+
+    pygame.display.update()
+
+
 
 buttons = [Button("Rock", 50, 500, (0, 0, 0)), Button("Scissors", 250, 500, (255, 0, 0)),  Button("Paper", 250, 500, (0, 255, 0))]
 
